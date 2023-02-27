33e4fffbfcd53831f77aa7d6947da4128305cd06
sirpy <hadarr@gmail.com>
2023-02-27 11:20:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,13 @@ hosts=$(curl --request GET \
         
 hostId=$(jq -r ".result | map(select(.name == \"${RECORD_DOMAIN}\")) | .[0].id" <<< "${hosts}" )
 
+if [ -z "${hostId}"]; then
+  echo "Host id for $RECORD_DOMAIN not found";
+  exit 1;
+fi
+
+echo "Found host id for $RECORD_DOMAIN: $hostId";
+
 response=$(curl --request PATCH \
         -H "Authorization: Bearer $CLOUDFLARE_TOKEN" \
         -H "Content-Type: application/json" \
@@ -17,12 +24,12 @@ response=$(curl --request PATCH \
 success=$(jq -r  '.success' <<< "${response}" )
 
 if [ $success != "true" ]; then
-  echo "Pages Update: Failed to update record!"
+  echo "Web3 Hostname Update: Failed to update record!"
   errors=$(jq -r  '.errors' <<< "${response}" )
   echo "Errors: $errors"
   exit 1
 fi
 
-echo "Pages Update: Success"
+echo "Web3 Hostname Update: Success"
 echo "  Webpage $RECORD_NAME updated to $1."
 echo "  Update time will vary depending on your Cloudflare settings."
